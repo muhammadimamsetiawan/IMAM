f0a64c0ec3c8a757afa6ef0a4284b96e33aba2db
Muhammad Imam Setiawan <imamsetiawan27062000@gmail.com>
2021-03-16 09:17:49 +0700

    commite Membuat Variabel dalam dart

---
+++ dasar-dart.dart
@@ -0,0 +1,15 @@
+void main(){
+    //Membuat Variabel dengan tipe data
+    String nama = "Imam"
+    int umur = 20;
+    double tinggi = 170.55;
+    bool disMenikah = false;
+    
+    //membuat Variabel dengan kata kunci var
+    var alamat = "Bengkalis, Riau";
+
+    //mencetak Variabel
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.")
+    print("Menikah : $isMenikah")
+    print("Alamat : $alamat");
+}
